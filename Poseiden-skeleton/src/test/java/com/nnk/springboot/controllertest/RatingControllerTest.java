7df8b137d271df3fcf0275ebeb330a4f5d33a023
Jean-Benoit <jean.benoit.roussat@gmail.com>
2020-07-08 11:50:46 +0200

    ajout delete controller rating

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/controllertest/RatingControllerTest.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllertest/RatingControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -32,6 +33,7 @@ public class RatingControllerTest {
 	}
 
 	@Test
+	@WithUserDetails("user")
 	public void postRating() throws Exception {
 		this.mockMvc.perform(MockMvcRequestBuilders.post("/rating/add").contentType(MediaType.APPLICATION_JSON).content(
 				"{\"moodysRating\":\"moodysRating\",\"sandPRating\":\"sandPRating\",\"fitchRating\":\"fitchRating\", \"orderNumber\":10}")
@@ -39,18 +41,28 @@ public class RatingControllerTest {
 	}
 
 	@Test
+	@WithUserDetails("user")
 	public void getRating() throws Exception {
 		this.postRating();
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/rating/list").contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isMethodNotAllowed());
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
 	}
 
 	@Test
+	@WithUserDetails("user")
 	public void updateRating() throws Exception {
 		this.postRating();
 		this.mockMvc.perform(MockMvcRequestBuilders.put("/rating/update/1").contentType(MediaType.APPLICATION_JSON)
 				.content(
 						"{\"moodysRating\":\"moodysRating\",\"sandPRating\":\"sandPRating\",\"fitchRating\":\"fitchRating\", \"orderNumber\":100}")
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isMethodNotAllowed());
+	}
+
+	@Test
+	@WithUserDetails("user")
+	public void deleteRating() throws Exception {
+		this.postRating();
+		this.mockMvc.perform(MockMvcRequestBuilders.delete("/rating/delete/1").contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isMethodNotAllowed());
 	}
 
