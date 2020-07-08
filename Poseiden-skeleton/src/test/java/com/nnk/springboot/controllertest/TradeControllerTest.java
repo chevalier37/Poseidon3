53b96b901aac456507df178cd3cda752cc0c395f
Jean-Benoit <jean.benoit.roussat@gmail.com>
2020-07-08 12:01:21 +0200

    Ajout controller trade

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllertest/TradeControllerTest.java
@@ -0,0 +1,70 @@
+package com.nnk.springboot.controllertest;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class TradeControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	@WithUserDetails("user")
+	public void postTrade() throws Exception {
+		this.mockMvc
+				.perform(MockMvcRequestBuilders.post("/trade/add").contentType(MediaType.APPLICATION_JSON)
+						.content("{\"account\":\"account\",\"type\":\"type\"}").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isMethodNotAllowed());
+	}
+
+	@Test
+	@WithUserDetails("user")
+	public void getRuleName() throws Exception {
+		this.postTrade();
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/trade/list").contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+	}
+
+	@Test
+	@WithUserDetails("user")
+	public void updateRuleName() throws Exception {
+		this.postTrade();
+		this.mockMvc
+				.perform(MockMvcRequestBuilders.put("/trade/update/1").contentType(MediaType.APPLICATION_JSON)
+						.content("{\"account\":\"account\",\"type\":\"type1\"}").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isMethodNotAllowed());
+	}
+
+	@Test
+	@WithUserDetails("user")
+	public void deleteRuleName() throws Exception {
+		this.postTrade();
+		this.mockMvc.perform(MockMvcRequestBuilders.delete("/trade/delete/1").contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isMethodNotAllowed());
+	}
+
+}
